db1a7478e5f654560662caac354b041aa10a2d18
matheus-fls <matheus_fls@yahoo.com.br>
2019-12-11 17:31:46 -0300

    Fix more errors

--- src/js/api.js
+++ src/js/api.js
@@ -3,7 +3,7 @@ import render from './view';
 async function getWeather() {
   try {
     const location = document.getElementById('search').value;
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ location }&appid=a9b08ec198a19ece08c7d0379994415b`, {mode: 'cors'});
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a9b08ec198a19ece08c7d0379994415b`, { mode: 'cors' });
     const weatherData = await response.json();
     render(weatherData);
   } catch (err) {
