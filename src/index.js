0300ea48aa3ae7c7c6ed932f1aadecca332983c8
matheus-fls <matheus_fls@yahoo.com.br>
2019-12-11 17:27:59 -0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.scss';
-import "regenerator-runtime/runtime";
+import 'regenerator-runtime/runtime';
 import getWeather from './js/api';
 
 document.getElementById('submit').addEventListener('click', getWeather);
--- src/js/view.js
+++ src/js/view.js
@@ -2,7 +2,7 @@ import temperature from './logic';
 
 const render = (data) => {
   const temp = temperature(data.main.temp);
-  document.getElementById('content').setAttribute("class", "light-border");
+  document.getElementById('content').setAttribute('class', 'light-border');
   document.getElementById('content').innerHTML = `
     <h2>${data.name}, ${data.sys.country}</h2>
     <h4>${data.weather[0].description}</h4>
