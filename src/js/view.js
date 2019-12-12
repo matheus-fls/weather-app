c395af37c09b895e9925bd401c931db45fff179a
matheus-fls <matheus_fls@yahoo.com.br>
2019-12-12 17:34:07 -0300

    Remove babel, fix border bug

--- src/js/view.js
+++ src/js/view.js
@@ -1,11 +1,11 @@
 const render = (data) => {
-  document.getElementById('content').setAttribute('class', 'light-border');
   document.getElementById('content').innerHTML = `
     <h2>${data.name}, ${data.sys.country}</h2>
     <h4>${data.weather[0].description}</h4>
     <h3 id="temp">${data.main.temp}</h3>
   `;
   document.getElementById('temp').innerHTML += document.querySelector('input[name="unit"]:checked').value === 'celsius' ? ' °C' : ' °F';
+  document.getElementById('content').setAttribute('class', 'light-border');
 };
 
 export default render;
