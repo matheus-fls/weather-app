8349ebc82413a5145ea45eedf4ff7827eab18c08
matheus-fls <matheus_fls@yahoo.com.br>
2019-12-13 12:46:58 -0300

    Add icons for each weather condition

--- src/js/view.js
+++ src/js/view.js
@@ -1,7 +1,13 @@
 const render = (data) => {
   document.getElementById('content').innerHTML = `
     <h2>${data.name}, ${data.sys.country}</h2>
-    <h4>${data.weather[0].description}</h4>
+    <div>
+      <img id="icon" 
+        src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"
+        alt="${data.weather[0].description} icon"  
+      >
+      <h4>${data.weather[0].description}<h4>
+    </div>
     <h3 id="temp">${data.main.temp}</h3>
   `;
   document.getElementById('temp').innerHTML += document.querySelector('input[name="unit"]:checked').value === 'celsius' ? ' °C' : ' °F';
